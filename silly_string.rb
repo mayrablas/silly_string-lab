30697e66c94e87293c6b010181458e3e724b41da
Mayra Blas <mayra.blas28@gmail.com>
2017-06-20 17:12:03 +0000

    moved all food back to places, still need to move cockroaches

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,15 @@
 #code your solutions here
+"my word".upcase
+ "IM NOT SHOUTING".downcase
+"Karlie Kloss" + " wow " + " mom"
 
+"i really like programming".capitalize
+"Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub(/[A]/,'O').gsub(/[a]/,'o')
 
-
+co="Chicago"
+la="Los Angeles"
+ny="New York"
+stl="Saint Louis"
+dt="Detroit"
+"Oakland"
+"Austin"
